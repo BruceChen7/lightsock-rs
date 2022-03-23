a1b4d3ee1f41ea1f16cbe07df00304d46e125a00
BruceChen7 <bofengqiye7@gmail.com>
2022-03-23 22:36:07 +0800

    feat(lightsock-rs): add more impl

--- src/client.rs
+++ src/client.rs
@@ -5,7 +5,7 @@ use bytes::{BufMut, Bytes, BytesMut};
 use tokio::{
     io::BufWriter,
     net::TcpStream,
-    sync::{broadcast, broadcast::Sender, oneshot},
+    sync::{broadcast, broadcast::Sender, mpsc, oneshot},
 };
 
 type Response<T> = oneshot::Sender<crate::Result<T>>;
@@ -17,6 +17,7 @@ struct Connection {
     buffer: BytesMut,
     job_receiver: async_channel::Receiver<ReqTask>,
     shutdown: Shutdown,
+    shutdown_complete: mpsc::Sender<()>,
 }
 
 impl Connection {
@@ -24,12 +25,14 @@ impl Connection {
         socket: TcpStream,
         job_receiver: async_channel::Receiver<ReqTask>,
         notify: broadcast::Receiver<()>,
+        shutdown_complete: mpsc::Sender<()>,
     ) -> Connection {
         Self {
             stream: BufWriter::new(socket),
             buffer: BytesMut::with_capacity(4 * 1024),
             shutdown: Shutdown::new(notify),
             job_receiver,
+            shutdown_complete,
         }
     }
 
@@ -67,32 +70,52 @@ impl Client {
 }
 
 struct ConnectionPool {
+    size: i32,
     job_signal: async_channel::Sender<ReqTask>,
     shutdown_sender: broadcast::Sender<()>,
+    shutdown_complete: mpsc::Receiver<()>,
 }
 
 impl ConnectionPool {
     pub async fn new(s: i32, addr: &str) -> crate::Result<ConnectionPool> {
         let (job_sender, job_receive) = async_channel::unbounded();
         let (shutdown_sender, _) = broadcast::channel(1);
+        let (connection_shutdown_complete, connection_shutdown_recv) = mpsc::channel(s as usize);
 
         for _ in 0..s {
             let socket = TcpStream::connect(addr).await?;
-            let connection =
-                Connection::new(socket, job_receive.clone(), shutdown_sender.subscribe());
-            tokio::spawn(async move {
-                connection.start().await;
-            });
+            let connection = Connection::new(
+                socket,
+                job_receive.clone(),
+                shutdown_sender.subscribe(),
+                connection_shutdown_complete.clone(),
+            );
+            let h = tokio::spawn(async move { connection.start().await });
+            h.await.unwrap()
         }
         Ok(Self {
+            size: s,
             job_signal: job_sender,
             shutdown_sender,
+            shutdown_complete: connection_shutdown_recv,
         })
     }
 
     pub async fn add_task(&self, task: ReqTask) -> crate::Result<bool> {
         self.job_signal.send(task).await?;
         Ok(true)
+    }
+
+    pub async fn shutdown(&mut self) -> crate::Result<()> {
+        let mut i = 0;
+        self.shutdown_sender.send(())?;
+        loop {
+            if self.size == i {
+                return Ok(());
+            }
+            self.shutdown_complete.recv().await;
+            i = i + 1
+        }
     }
 }
 
